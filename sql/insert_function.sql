d1a4a6d16cec8a7ceca4953fdf1602423d8d44e3
atalmo <65801654+atalmo@users.noreply.github.com>
2020-05-23 13:58:26 +0800

    Update insert_function.sql

--- sql/insert_function.sql
+++ sql/insert_function.sql
@@ -1,27 +1,27 @@
-DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text);
+DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text, text, text, text) ;
 --Assumes only one value being inserted
 
 CREATE OR REPLACE FUNCTION insert_crowd_mapping_data (
     _geojson TEXT,
-    _name TEXT,
-    _note TEXT,	
-    _hear TEXT)    
+    _location TEXT,
+    _water_color TEXT,	
+    _date TEXT)    
 --Has to return something in order to be used in a "SELECT" statement
 RETURNS integer
 AS $$
 DECLARE 
     _the_geom GEOMETRY;
 	--The name of your table in cartoDB
-	_the_table TEXT := 'hearhere';
+	_the_table TEXT := 'reported_hab_incident';
 BEGIN
     --Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
 	
 
 	--Executes the insert given the supplied geometry, description, and username, while protecting against SQL injection.
-    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, name, note, hear)
+    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, location, water_color, date)
             VALUES ($1, $2, $3, $4)
-            ' USING _the_geom, _name, _note, _hear;
+            ' USING _the_geom, _location, _water_color, _date;
             
     RETURN 1;
 END;
